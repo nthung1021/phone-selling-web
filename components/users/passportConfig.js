b38c641c061bb20ef035a15cd762ff830e1368da
nthung1021 <nthung22@clc.fitus.edu.vn>
2024-12-02 18:52:10 +0700

    Update passportConfig.js

--- components/users/passportConfig.js
+++ components/users/passportConfig.js
@@ -9,12 +9,12 @@ passport.use(new LocalStrategy(async (username, password, done) => {
   try {
     var user = await findUserByUsername(username);
     if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
+      return done(null, false, { message: 'Incorrect username' });
     }
 
     var isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return done(null, false, { message: 'Incorrect password.' });
+      return done(null, false, { message: 'Incorrect password' });
     }
 
     return done(null, user);
