3cccddc7e3ee88544f948407992a24c264bc0c7f
nthung1021 <nthung22@clc.fitus.edu.vn>
2024-12-20 17:42:03 +0700

    Update usersRoute.js

--- components/users/usersRoute.js
+++ components/users/usersRoute.js
@@ -1,10 +1,12 @@
 var express = require('express');
-var { getRegister, postRegister, getLogin, postLogin, getInfo, getLogout, ensureAuthenticated, checkAvailability } = require('./usersController');
+var { getRegister, postRegister, getLogin, postLogin, getInfo,
+getLogout, ensureAuthenticated, checkAvailability, forgotPassword } = require('./usersController');
 var router = express.Router();
 
 router.get('/register', getRegister);
 router.post('/register', postRegister);
 router.get('/login', getLogin);
+router.get('/forgot-password', forgotPassword);
 router.post('/login', postLogin);
 router.get('/logout', getLogout);
 router.get('/info', ensureAuthenticated, getInfo)
