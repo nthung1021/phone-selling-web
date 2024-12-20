5d87461b98542c69edc62906e096aef52c24c4b4
nthung1021 <nthung22@clc.fitus.edu.vn>
2024-12-20 17:36:14 +0700

    Update indexModel.js

--- components/index/indexModel.js
+++ components/index/indexModel.js
@@ -1,16 +1,29 @@
 var {PrismaClient} = require("@prisma/client");
 var prisma = new PrismaClient();
 
-var getFeaturedProducts = async () => {
+// var getFeaturedProducts = async () => {
+//     try {
+//         return prisma.product.findMany({
+//             where: { isFeatured: true },
+//             take: 8,
+//         });
+//     } catch (error) {
+//         console.error('Error fetching featured products:', error);
+//         throw error;
+//     }   
+// };
+
+
+const getFeaturedProducts = async () => {
     try {
-        return prisma.product.findMany({
-            where: { isFeatured: true },
-            take: 8,
+        const featuredProducts = await prisma.product.findMany({
+            where: { isFeatured: true }
         });
+        return featuredProducts;
     } catch (error) {
-        console.error('Error fetching featured products:', error);
+        console.error('Error finding featured products:', error);
         throw error;
-    }   
+    }
 };
 
 module.exports = {getFeaturedProducts};
