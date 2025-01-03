fd288196950cbd6f8e3f24337b9f3ae339614b7c
Noru-0 <htphat22@clc.fitus.edu.vn>
2025-01-03 18:07:47 +0700

    Check status login

--- components/users/usersController.js
+++ components/users/usersController.js
@@ -3,67 +3,70 @@ var passport = require('./passportConfig');
 var { findUserByUsername, createUser, findUserByEmail } = require('./usersModel');
 
 var getLogin = (req, res) => {
-    res.render('login', { title: 'Log in' });
+  res.render('login', { title: 'Log in' });
 };
 
 var postLogin = (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return res.render('login', { error: 'An unexpected error occurred. Please try again.', title: 'Login' });
+    }
+    if (!user) {
+      // `info.message` contains the error message set in `passportConfig.js`
+      return res.render('login', { error: info.message, title: 'Login', username: req.body.username });
+    }
+    if (!user.status) {
+      return res.render('login', { error: 'Your account is inactive. Please contact support.', layout: 'main' });
+    }
+    req.logIn(user, (err) => {
       if (err) {
         console.error(err);
-        return res.render('login', { error: 'An unexpected error occurred. Please try again.', title: 'Login' });
+        return res.render('login', { error: 'Failed to log in. Please try again.', title: 'Login' });
       }
-      if (!user) {
-        // `info.message` contains the error message set in `passportConfig.js`
-        return res.render('login', { error: info.message, title: 'Login', username: req.body.username });
-      }
-      req.logIn(user, (err) => {
-        if (err) {
-          console.error(err);
-          return res.render('login', { error: 'Failed to log in. Please try again.', title: 'Login' });
-        }
-        return res.redirect('/');
-      });
-    })(req, res, next);
-  };
+      return res.redirect('/');
+    });
+  })(req, res, next);
+};
 
 var getRegister = (req, res) => {
-    res.render('register', { title: 'Register' });
+  res.render('register', { title: 'Register' });
 };
 
 var postRegister = async (req, res) => {
-    var { username, email, password, confirmPassword } = req.body;
-  
-    if (!username || !password || !email || !confirmPassword) {
-        return res.render('register', { error: 'All fields are required', title: 'Register' });
+  var { username, email, password, confirmPassword } = req.body;
+
+  if (!username || !password || !email || !confirmPassword) {
+    return res.render('register', { error: 'All fields are required', title: 'Register' });
   }
 
-    try {
-        var existingUser = await findUserByUsername(username);
-        var existingEmail = await findUserByEmail(email);
+  try {
+    var existingUser = await findUserByUsername(username);
+    var existingEmail = await findUserByEmail(email);
 
-        if (existingUser) {
-            return res.render('register', { error: 'Username already exists', title: 'Register' });
-        }
-        
-        if (existingEmail) {
-            return res.render('register', { error: 'Email already exists', title: 'Register' });
-        }
-
-        if (password !== confirmPassword) {
-            return res.render('register', { error: 'Confirm password must be the same as passwword', title: 'Register'});
-        }
-
-        var hashedPassword = await bcrypt.hash(password, 10);
-        await createUser(username, email, hashedPassword);
-
-        res.render('register', {
-          success: 'Registration successful! Please log in.',
-          title: 'Register'
-        });
-      } catch (error) {
-        console.error(error);
-        res.render('register', { error: 'Something went wrong!', title: 'Register' });
+    if (existingUser) {
+      return res.render('register', { error: 'Username already exists', title: 'Register' });
     }
+
+    if (existingEmail) {
+      return res.render('register', { error: 'Email already exists', title: 'Register' });
+    }
+
+    if (password !== confirmPassword) {
+      return res.render('register', { error: 'Confirm password must be the same as passwword', title: 'Register' });
+    }
+
+    var hashedPassword = await bcrypt.hash(password, 10);
+    await createUser(username, email, hashedPassword);
+
+    res.render('register', {
+      success: 'Registration successful! Please log in.',
+      title: 'Register'
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('register', { error: 'Something went wrong!', title: 'Register' });
+  }
 };
 
 var forgotPassword = (req, res) => {
@@ -71,32 +74,32 @@ var forgotPassword = (req, res) => {
 };
 
 function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        // Create an error object to pass to the view
-        const error = {
-            status: 401,
-            message: 'Unauthorized access. Please log in to continue.',
-            stack: (new Error()).stack // Optional: include stack trace if needed
-        };
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    // Create an error object to pass to the view
+    const error = {
+      status: 401,
+      message: 'Unauthorized access. Please log in to continue.',
+      stack: (new Error()).stack // Optional: include stack trace if needed
+    };
 
-        // Render the error page and pass the error object
-        res.status(401).render('error', { error });
-    }
+    // Render the error page and pass the error object
+    res.status(401).render('error', { error });
+  }
 }
 
 const getInfo = (req, res) => {
-    res.render('info', { title: 'Information'});
+  res.render('info', { title: 'Information' });
 }
 
 const getLogout = async (req, res, next) => {
-    req.logout(err => {
-        if (err) {
-            return next(err); 
-        }
-        res.redirect('/');
-    });
+  req.logout(err => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 };
 
 const checkAvailability = async (req, res) => {
@@ -111,7 +114,7 @@ const checkAvailability = async (req, res) => {
 
     if (email) {
       const user = await findUserByEmail(email);
-      const emailExists = user !== null; 
+      const emailExists = user !== null;
       return res.json({ exists: emailExists });
     }
 
@@ -122,5 +125,7 @@ const checkAvailability = async (req, res) => {
   }
 };
 
-module.exports = { getLogin, postLogin, getRegister, postRegister, 
-getInfo, getLogout, ensureAuthenticated, checkAvailability, forgotPassword };
+module.exports = {
+  getLogin, postLogin, getRegister, postRegister,
+  getInfo, getLogout, ensureAuthenticated, checkAvailability, forgotPassword
+};
