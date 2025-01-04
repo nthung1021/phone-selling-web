a3f0c1f26e48ee5b19d55e3577bda68365eb9e95
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-01-05 00:39:43 +0700

    Delete unused code (indexModel.js)

--- components/index/indexModel.js
+++ components/index/indexModel.js
@@ -1,19 +1,6 @@
 var {PrismaClient} = require("@prisma/client");
 var prisma = new PrismaClient();
 
-// var getFeaturedProducts = async () => {
-//     try {
-//         return prisma.product.findMany({
-//             where: { isFeatured: true },
-//             take: 8,
-//         });
-//     } catch (error) {
-//         console.error('Error fetching featured products:', error);
-//         throw error;
-//     }   
-// };
-
-
 const getFeaturedProducts = async () => {
     try {
         const featuredProducts = await prisma.product.findMany({
