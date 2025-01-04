51a2b0f1008a62bccd3c2700bda8f964bc530476
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-01-05 00:48:23 +0700

    Add feature to update cart from guest to logged-in user

--- components/users/passportConfig.js
+++ components/users/passportConfig.js
@@ -6,27 +6,39 @@ var { PrismaClient } = require('@prisma/client');
 var prisma = new PrismaClient();
 
 passport.use(new LocalStrategy(async (username, password, done) => {
-  try {
-    var user = await findUserByUsername(username);
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username' });
-    }
+    try {
+        var user = await findUserByUsername(username);
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username' });
+        }
 
-    var isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return done(null, false, { message: 'Incorrect password' });
-    }
+        var isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
 
-    return done(null, user);
-  } catch (error) {
-    return done(error);
-  }
+        return done(null, user);
+    } catch (error) {
+        return done(error);
+    }
 }));
 
 // Serialize user to save in session
-passport.serializeUser((user, done) => {
-  done(null, user.id);
+passport.serializeUser(async (user, done) => {
+  try {
+      // Update guest cart items to associate with logged-in user
+      await prisma.cart.updateMany({
+          where: { sessionId: user.sessionID },
+          data: { userId: user.id, sessionId: null }, // Assign the logged-in user's ID and clear sessionId
+      });
+
+      done(null, user.id);
+  } catch (error) {
+      console.error('Error updating cart items after login:', error);
+      done(error, null);
+  }
 });
+
 
 // Deserialize user from session
 passport.deserializeUser(async (id, done) => {
