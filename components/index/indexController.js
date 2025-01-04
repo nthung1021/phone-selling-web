cc5cb9e6ca23de1dc6767ceb0fd9765b9ce32bbe
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-01-05 00:38:52 +0700

    Delete unused code (indexController.js)

--- components/index/indexController.js
+++ components/index/indexController.js
@@ -1,23 +1,5 @@
 var {getFeaturedProducts} = require("./indexModel");
 
-// var showFeaturedProducts = async (req, res) => {
-//     try {
-//         var featuredProducts = await getFeaturedProducts();
-
-//         featuredProducts = featuredProducts.map(product => ({
-//             ...product,
-//             discountedPrice: product.promotion
-//             ? (product.price * (1 - product.promotion / 100)).toFixed(2)
-//             : null,
-//         }));
-
-//         res.render('index', {featuredProducts, title: "GA05 - Home"});
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).render('error', { message: 'Something went wrong' });
-//     }
-// }
-
 var showFeaturedProducts = async (req, res) => {
     try {
         // Find products where isFeatured is true
