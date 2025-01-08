ac0ceac01c9351cd69d7619f3b5d691631ed6261
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-01-08 21:12:12 +0700

    Add functions for Reset Password feature + Modify finding email function

--- components/users/usersModel.js
+++ components/users/usersModel.js
@@ -13,8 +13,7 @@ var findUserByEmail = async (email) => {
     if (!email) {
         throw new Error('Email is required');
     }
-
-    return prisma.user.findUnique({ where: { email } });
+    return prisma.user.findFirst({ where: { email, googleId: null } });  
 };
 
 var createUser = async (username, email, hashedPassword) => {
@@ -23,4 +22,60 @@ var createUser = async (username, email, hashedPassword) => {
     });
 };
 
-module.exports = { findUserByUsername, createUser, findUserByEmail };
+var addTokenAndExpire = async (email, resetToken, expires) => {
+    if (!email) {
+        throw new Error('Email is required');
+    }
+    return prisma.user.update({
+        where: { email },
+        data: {
+            resetPasswordToken: resetToken,
+            resetPasswordExpires: expires,
+        },
+    });
+}
+
+var findTokenAndExpire = async (token) => {
+    if (!token) {
+        throw new Error('Reset-password token is empty');
+    }
+    return prisma.user.findFirst({
+        where: {
+            resetPasswordToken: token,
+            resetPasswordExpires: {
+                gte: new Date(), // Token chưa hết hạn
+            },
+        },
+    });
+}
+
+var updatePassword = async (userid, hashedPassword) => {
+    return prisma.user.update({
+        where: { id: userid },
+        data: {
+            password: hashedPassword,
+            resetPasswordToken: null,
+            resetPasswordExpires: null,
+        },
+    });
+}
+
+var findOrCreateGoogleUser = async (googleId, username, email) => {
+    let user = await prisma.user.findUnique({ where: { googleId } });
+    if (!user) {
+        user = await prisma.user.create({
+            data: {
+                googleId,
+                username,
+                email,
+            },
+        });
+    }
+    return user;
+};
+
+module.exports = {
+    findUserByUsername, createUser, findUserByEmail, 
+    addTokenAndExpire, findTokenAndExpire, updatePassword,
+    findOrCreateGoogleUser
+};
