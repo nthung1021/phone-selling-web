3ef0ae7a03ede9ba0bda04c93693a2a76ced2460
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-01-08 21:10:08 +0700

    Add functions for Reset Password feature

--- components/users/usersController.js
+++ components/users/usersController.js
@@ -1,6 +1,11 @@
 var bcrypt = require('bcrypt');
 var passport = require('./passportConfig');
-var { findUserByUsername, createUser, findUserByEmail } = require('./usersModel');
+var nodemailer = require('nodemailer');
+var crypto = require('crypto');
+var {
+    findUserByUsername, createUser, findUserByEmail, 
+    addTokenAndExpire, findTokenAndExpire, updatePassword
+} = require('./usersModel');
 
 var getLogin = (req, res) => {
   res.render('login', { title: 'Log in' });
@@ -69,8 +74,109 @@ var postRegister = async (req, res) => {
   }
 };
 
-var forgotPassword = (req, res) => {
-  res.render('forgot-password', { title: 'Forgot Password' });
+var getForgotPassword = (req, res) => {
+    res.render('forgot-password', { title: 'Forgot Password' });
+};
+
+const postForgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        res.render('forgot-password', {title: 'Forgot Password', error: "Please enter an email"});
+    }
+
+    try {
+        // Find user based on the input email
+        const user = await findUserByEmail(email);
+
+        // If user if not found, return error
+        if (!user) {
+            res.render('forgot-password', {title: 'Forgot Password', error: "Email is not registered"});
+        }
+
+        // Generate reset-password token
+        const resetToken = generateResetToken();
+
+        const expires = new Date();
+        expires.setHours(expires.getHours() + 1); // Set token expire time to 1 hour
+
+        await addTokenAndExpire(email, resetToken, expires);
+
+        // Send email and reset-password link with endpoint is token
+        const resetLink = req.protocol + '://' + req.get('host') + '/users/reset-password/' + resetToken;
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com', // Can use other account
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_SENDER, // Real account
+                pass: process.env.APP_PASSWORD, // Password of the app (not account password)
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_SENDER,
+            to: email,
+            subject: 'Password Reset Request',
+            text: `Click the link below to reset your password:\n\n${resetLink}\n\nThe link will expire in 1 hour.`,
+        };
+
+        // Send email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                res.render('forgot-password', {title: 'Forgot Password', error: 'Failed to send request email. Please try again later'});
+            } else {
+                res.render('forgot-password', {title: 'Forgot Password', success: 'Password reset link has been sent to your email'})
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.render('forgot-password', {title: 'Forgot Password', error: 'Something went wrong. Please try again later.'});
+    }
+};
+
+const getResetPassword = async (req, res) => {
+    const { token } = req.params;
+
+    try {
+        const user = await findTokenAndExpire(token);
+        if (!user) {
+            res.render('forgot-password', {title: 'Forgot Password', error: 'Invalid or expired token'});
+        }
+
+        res.render('reset-password', {title: 'Reset Password', token});
+    } catch (error) {
+        console.error(error);
+        res.render('forgot-password', {title: 'Forgot Password', error: 'Something went wrong. Please try again later'});
+    }
+};
+
+const postResetPassword = async (req, res) => {
+    const { token, password, confirmPassword } = req.body;
+
+    if (!password || !confirmPassword || password !== confirmPassword) {
+        res.render('reset-password', {title: 'Reset Password', error: 'Passwords do not match.', token});
+    }
+
+    try {
+        const user = await findTokenAndExpire(token);
+        if (!user) {
+            res.render('forgot-password', {title: 'Forgot Password', error: 'Invalid or expired token'});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await updatePassword(user.id, hashedPassword);
+
+        res.render('forgot-password', {title: 'Reset Password', success: 'Password updated successfully. You can now log in.'});
+    } catch (error) {
+        console.error(error);
+        res.render('reset-password', {title: 'Reset Password', error: 'Something went wrong. Please try again later.', token});
+    }
+};
+
+const generateResetToken = () => {
+  return crypto.randomBytes(32).toString('hex');
 };
 
 function ensureAuthenticated(req, res, next) {
@@ -127,5 +233,6 @@ const checkAvailability = async (req, res) => {
 
 module.exports = {
   getLogin, postLogin, getRegister, postRegister,
-  getInfo, getLogout, ensureAuthenticated, checkAvailability, forgotPassword
+  getInfo, getLogout, ensureAuthenticated, checkAvailability, 
+  getForgotPassword, postForgotPassword, getResetPassword, postResetPassword
 };
