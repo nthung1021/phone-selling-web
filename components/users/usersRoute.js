87332573fd2c8e421b28f355e551643d276011ae
nthung1021 <nthung22@clc.fitus.edu.vn>
2025-01-08 21:12:44 +0700

    Add routes for Reset Password + Google Login

--- components/users/usersRoute.js
+++ components/users/usersRoute.js
@@ -1,15 +1,38 @@
 var express = require('express');
-var { getRegister, postRegister, getLogin, postLogin, getInfo,
-getLogout, ensureAuthenticated, checkAvailability, forgotPassword } = require('./usersController');
+var passport = require('passport');
+var { 
+    getRegister, postRegister, getLogin, postLogin, getInfo,
+    getLogout, ensureAuthenticated, checkAvailability, getForgotPassword,
+    postForgotPassword, getResetPassword, postResetPassword
+} = require('./usersController');
 var router = express.Router();
 
 router.get('/register', getRegister);
 router.post('/register', postRegister);
+
 router.get('/login', getLogin);
-router.get('/forgot-password', forgotPassword);
 router.post('/login', postLogin);
+
 router.get('/logout', getLogout);
+
+router.get('/forgot-password', getForgotPassword);
+router.post('/forgot-password', postForgotPassword);
+
+router.get('/reset-password/:token', getResetPassword);
+router.post('/reset-password/:token', postResetPassword);
+
 router.get('/info', ensureAuthenticated, getInfo)
 router.get('/check-availability', checkAvailability);
+
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+router.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/users/login',
+        successRedirect: '/'
+    })
+);
 
 module.exports = router;
